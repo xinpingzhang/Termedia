c04cd0c96252a0fa3a0f075366b21f65e8083541
Kelvin Zhang <zhangxp1998@gmail.com>
2017-12-05 19:41:51 -0500

    Initial Commit

---
+++ Termedia/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Termedia
+//
+//  Created by Kelvin Zhang on 12/5/17.
+//  Copyright © 2017 Kelvin Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
